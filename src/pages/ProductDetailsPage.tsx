faf73738198b95a443fed809cb309b7979539953
Max Reichenauer <max.reichenauer98@gmail.com>
2024-08-12 09:04:12 +0200

    Added usescrolltoTop inside productdetailspage

--- src/pages/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useSingleProduct } from "../hooks/useSingleProduct";
 import Container from "react-bootstrap/Container";
 import DetailCard from "../components/cards/DetailCard";
+import useScrollToTop from "../hooks/utils/useScrollToTop";
 
 const ProductDetailsPage = () => {
 	const { id } = useParams();
@@ -13,6 +14,8 @@ const ProductDetailsPage = () => {
 		error: productError,
 		isLoading: productIsLoading,
 	} = useSingleProduct(productId);
+
+	useScrollToTop(id as string);
 
 	if (productIsLoading) {
 		return <p>Loading...</p>;
