dffcc3446cb9f6566dad38b2d2231b59b8c758f7
Max Reichenauer <max.reichenauer98@gmail.com>
2024-08-12 08:49:09 +0200

    Styled dropdown menu for sort with css

--- src/components/utils/SortProducts.tsx
+++ src/components/utils/SortProducts.tsx
@@ -1,6 +1,7 @@
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import { useEffect, useState } from "react";
+import "../../assets/scss/SortProducts.scss";
 
 type SortProductsProps = {
 	sortBy: string;
