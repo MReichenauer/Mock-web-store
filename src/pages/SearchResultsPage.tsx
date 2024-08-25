ff011b43055ec0c405f720ba003f0106f1a08e97
Max Reichenauer <max.reichenauer98@gmail.com>
2024-08-25 10:10:54 +0200

    Made a more describing ttile in searchResultsPage

--- src/pages/SearchResultsPage.tsx
+++ src/pages/SearchResultsPage.tsx
@@ -63,6 +63,7 @@ const SearchResultsPage = () => {
 		<Container>
 			{searchQuery && (
 				<h1 className="h2 searchQueryTitle">
+					Products matching:{" "}
 					{searchQuery.charAt(0).toUpperCase() + searchQuery.slice(1)}
 				</h1>
 			)}
